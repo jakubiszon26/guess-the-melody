dc5d76ebf8f6d85281e2b6c1cb5d90c701c31add
jakubiszon26 <jakubkolakowski6@protonmail.com>
2025-11-20 19:09:36 +0100

    vercel update

--- src/api/socket.js
+++ src/api/socket.js
@@ -1,19 +1,10 @@
-import { HttpStatusCode } from "axios";
 import { io } from "socket.io-client";
-import { httpUrl } from "zod";
 
-// const SOCKET_URL = import.meta.env.VITE_BACKEND_URL ?? "http://127.0.0.1:3001";
-
-const SOCKET_URL =
-  (typeof import.meta !== "undefined"
-    ? import.meta.env.VITE_BACKEND_URL
-    : process.env.VITE_BACKEND_URL) ?? "http://127.0.0.1:3001";
+const SOCKET_URL = "/";
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
-  withCredentials: true,
-  // Temporary until the api isnt Https
-  path: "/api/socket.io/",
+  path: "/api/socket.io",
   transports: ["polling"],
   withCredentials: true,
 });
