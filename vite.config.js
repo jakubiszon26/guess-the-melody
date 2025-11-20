6d97c9b92abfec1e8287f195d948b5c46dbceae2
jakubiszon26 <jakubkolakowski6@protonmail.com>
2025-11-20 19:34:26 +0100

    vite proxy

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig, loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  const backendUrl = env.VITE_BACKEND_URL ?? "http://127.0.0.1:3001";
 
+export default defineConfig(({ mode }) => {
   return {
     plugins: [react(), tailwindcss()],
     server: {
@@ -13,8 +11,19 @@ export default defineConfig(({ mode }) => {
       open: true,
       proxy: {
         "/api": {
-          target: backendUrl,
+          target: "http://127.0.0.1:3001",
           changeOrigin: true,
+          secure: false,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+          configure: (proxy, _options) => {
+            proxy.on("error", (err, _req, _res) => {
+              console.log("proxy error", err);
+            });
+            proxy.on("proxyReq", (proxyReq, req, _res) => {
+              console.log("Request do backendu:", req.method, req.url);
+            });
+          },
         },
       },
     },
